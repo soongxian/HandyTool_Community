0f1d0ac5c8e8a673d35704008ab8a8b46814e539
soongxian <soongxian@gmail.com>
2025-06-11 19:07:14 +0800

    Added database filter

--- HandyTool.Presentation/Tabs/SqlSeekerPage.Designer.cs
+++ HandyTool.Presentation/Tabs/SqlSeekerPage.Designer.cs
@@ -30,7 +30,6 @@ namespace HandyTool.Tabs.SqlSeeker
         /// </summary>
         private void InitializeComponent()
         {
-            ColorGlobal cg = new ColorGlobal();
             SearchBar = new TextBox();
             CheckBoxTables = new CheckBox();
             CheckBoxStoredProcedure = new CheckBox();
@@ -38,11 +37,11 @@ namespace HandyTool.Tabs.SqlSeeker
             CheckBoxFunctions = new CheckBox();
             TableLayoutMaster = new TableLayoutPanel();
             TableLayoutTitle = new TableLayoutPanel();
-            TableLayoutFilter = new TableLayoutPanel();
+            DatabaseComboBox = new ComboBox();
+            RefreshButton = new Button();
             FilterResultGridView = new DataGridView();
             TableLayoutMaster.SuspendLayout();
             TableLayoutTitle.SuspendLayout();
-            TableLayoutFilter.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)FilterResultGridView).BeginInit();
             SuspendLayout();
             // 
@@ -52,21 +51,21 @@ namespace HandyTool.Tabs.SqlSeeker
             SearchBar.Location = new Point(3, 2);
             SearchBar.Margin = new Padding(3, 2, 3, 2);
             SearchBar.Name = "SearchBar";
-            SearchBar.Size = new Size(844, 23);
+            SearchBar.Size = new Size(284, 23);
             SearchBar.TabIndex = 0;
             SearchBar.TextChanged += SearchBar_TextChanged;
             // 
             // CheckBoxTables
             // 
             CheckBoxTables.AutoSize = true;
-            CheckBoxTables.BackColor = cg.ColorTable;
+            CheckBoxTables.BackColor = Color.FromArgb(192, 255, 255);
             CheckBoxTables.Checked = true;
             CheckBoxTables.CheckState = CheckState.Checked;
-            CheckBoxTables.Dock = DockStyle.Fill;
-            CheckBoxTables.Location = new Point(3, 2);
+            CheckBoxTables.Dock = DockStyle.Top;
+            CheckBoxTables.Location = new Point(801, 2);
             CheckBoxTables.Margin = new Padding(3, 2, 3, 2);
             CheckBoxTables.Name = "CheckBoxTables";
-            CheckBoxTables.Size = new Size(126, 21);
+            CheckBoxTables.Size = new Size(139, 19);
             CheckBoxTables.TabIndex = 1;
             CheckBoxTables.Text = "Tables";
             CheckBoxTables.UseVisualStyleBackColor = false;
@@ -75,14 +74,14 @@ namespace HandyTool.Tabs.SqlSeeker
             // CheckBoxStoredProcedure
             // 
             CheckBoxStoredProcedure.AutoSize = true;
-            CheckBoxStoredProcedure.BackColor = cg.ColorStoredProcedure;
+            CheckBoxStoredProcedure.BackColor = Color.FromArgb(192, 255, 192);
             CheckBoxStoredProcedure.Checked = true;
             CheckBoxStoredProcedure.CheckState = CheckState.Checked;
-            CheckBoxStoredProcedure.Dock = DockStyle.Fill;
-            CheckBoxStoredProcedure.Location = new Point(135, 2);
+            CheckBoxStoredProcedure.Dock = DockStyle.Top;
+            CheckBoxStoredProcedure.Location = new Point(946, 2);
             CheckBoxStoredProcedure.Margin = new Padding(3, 2, 3, 2);
             CheckBoxStoredProcedure.Name = "CheckBoxStoredProcedure";
-            CheckBoxStoredProcedure.Size = new Size(180, 21);
+            CheckBoxStoredProcedure.Size = new Size(212, 19);
             CheckBoxStoredProcedure.TabIndex = 2;
             CheckBoxStoredProcedure.Text = "Stored Procedure";
             CheckBoxStoredProcedure.UseVisualStyleBackColor = false;
@@ -91,14 +90,14 @@ namespace HandyTool.Tabs.SqlSeeker
             // CheckBoxViews
             // 
             CheckBoxViews.AutoSize = true;
-            CheckBoxViews.BackColor = cg.ColorView;
+            CheckBoxViews.BackColor = Color.FromArgb(255, 255, 192);
             CheckBoxViews.Checked = true;
             CheckBoxViews.CheckState = CheckState.Checked;
-            CheckBoxViews.Dock = DockStyle.Fill;
-            CheckBoxViews.Location = new Point(321, 2);
+            CheckBoxViews.Dock = DockStyle.Top;
+            CheckBoxViews.Location = new Point(1164, 2);
             CheckBoxViews.Margin = new Padding(3, 2, 3, 2);
             CheckBoxViews.Name = "CheckBoxViews";
-            CheckBoxViews.Size = new Size(100, 21);
+            CheckBoxViews.Size = new Size(139, 19);
             CheckBoxViews.TabIndex = 3;
             CheckBoxViews.Text = "Views";
             CheckBoxViews.UseVisualStyleBackColor = false;
@@ -107,14 +106,14 @@ namespace HandyTool.Tabs.SqlSeeker
             // CheckBoxFunctions
             // 
             CheckBoxFunctions.AutoSize = true;
-            CheckBoxFunctions.BackColor = cg.ColorFunction;
+            CheckBoxFunctions.BackColor = Color.FromArgb(255, 224, 192);
             CheckBoxFunctions.Checked = true;
             CheckBoxFunctions.CheckState = CheckState.Checked;
-            CheckBoxFunctions.Dock = DockStyle.Fill;
-            CheckBoxFunctions.Location = new Point(427, 2);
+            CheckBoxFunctions.Dock = DockStyle.Top;
+            CheckBoxFunctions.Location = new Point(1309, 2);
             CheckBoxFunctions.Margin = new Padding(3, 2, 3, 2);
             CheckBoxFunctions.Name = "CheckBoxFunctions";
-            CheckBoxFunctions.Size = new Size(128, 21);
+            CheckBoxFunctions.Size = new Size(142, 19);
             CheckBoxFunctions.TabIndex = 4;
             CheckBoxFunctions.Text = "Functions";
             CheckBoxFunctions.UseVisualStyleBackColor = false;
@@ -132,50 +131,65 @@ namespace HandyTool.Tabs.SqlSeeker
             TableLayoutMaster.RowCount = 2;
             TableLayoutMaster.RowStyles.Add(new RowStyle(SizeType.Percent, 6.060606F));
             TableLayoutMaster.RowStyles.Add(new RowStyle(SizeType.Percent, 93.93939F));
-            TableLayoutMaster.Size = new Size(1424, 627);
+            TableLayoutMaster.Size = new Size(1460, 662);
             TableLayoutMaster.TabIndex = 6;
             // 
             // TableLayoutTitle
             // 
-            TableLayoutTitle.ColumnCount = 2;
-            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 60F));
-            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            TableLayoutTitle.ColumnCount = 7;
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 15F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            TableLayoutTitle.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
             TableLayoutTitle.Controls.Add(SearchBar, 0, 0);
-            TableLayoutTitle.Controls.Add(TableLayoutFilter, 1, 0);
+            TableLayoutTitle.Controls.Add(DatabaseComboBox, 1, 0);
+            TableLayoutTitle.Controls.Add(CheckBoxFunctions, 6, 0);
+            TableLayoutTitle.Controls.Add(CheckBoxViews, 5, 0);
+            TableLayoutTitle.Controls.Add(CheckBoxStoredProcedure, 4, 0);
+            TableLayoutTitle.Controls.Add(CheckBoxTables, 3, 0);
+            TableLayoutTitle.Controls.Add(RefreshButton, 2, 0);
             TableLayoutTitle.Dock = DockStyle.Fill;
             TableLayoutTitle.Location = new Point(3, 3);
             TableLayoutTitle.Name = "TableLayoutTitle";
             TableLayoutTitle.RowCount = 1;
-            TableLayoutTitle.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            TableLayoutTitle.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            TableLayoutTitle.Size = new Size(1418, 32);
+            TableLayoutTitle.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            TableLayoutTitle.Size = new Size(1454, 34);
             TableLayoutTitle.TabIndex = 0;
             // 
-            // TableLayoutFilter
+            // DatabaseComboBox
             // 
-            TableLayoutFilter.ColumnCount = 4;
-            TableLayoutFilter.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-            TableLayoutFilter.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 35F));
-            TableLayoutFilter.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            TableLayoutFilter.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
-            TableLayoutFilter.Controls.Add(CheckBoxFunctions, 3, 0);
-            TableLayoutFilter.Controls.Add(CheckBoxTables, 0, 0);
-            TableLayoutFilter.Controls.Add(CheckBoxStoredProcedure, 1, 0);
-            TableLayoutFilter.Controls.Add(CheckBoxViews, 2, 0);
-            TableLayoutFilter.Location = new Point(853, 3);
-            TableLayoutFilter.Name = "TableLayoutFilter";
-            TableLayoutFilter.RowCount = 1;
-            TableLayoutFilter.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            TableLayoutFilter.Size = new Size(558, 25);
-            TableLayoutFilter.TabIndex = 5;
+            DatabaseComboBox.Dock = DockStyle.Top;
+            DatabaseComboBox.FormattingEnabled = true;
+            DatabaseComboBox.Items.AddRange(new object[] { "All (Refresh to see Db)" });
+            DatabaseComboBox.Location = new Point(293, 3);
+            DatabaseComboBox.Name = "DatabaseComboBox";
+            DatabaseComboBox.Size = new Size(430, 23);
+            DatabaseComboBox.TabIndex = 5;
+            DatabaseComboBox.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Dock = DockStyle.Top;
+            RefreshButton.Font = new Font("Wingdings 3", 8F);
+            RefreshButton.Location = new Point(729, 3);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(66, 23);
+            RefreshButton.TabIndex = 6;
+            RefreshButton.Text = "Q";
+            RefreshButton.UseVisualStyleBackColor = true;
+            RefreshButton.Click += RefreshButton_Click;
             // 
             // FilterResultGridView
             // 
             FilterResultGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             FilterResultGridView.Dock = DockStyle.Fill;
-            FilterResultGridView.Location = new Point(3, 41);
+            FilterResultGridView.Location = new Point(3, 43);
             FilterResultGridView.Name = "FilterResultGridView";
-            FilterResultGridView.Size = new Size(1418, 583);
+            FilterResultGridView.RowHeadersWidth = 51;
+            FilterResultGridView.Size = new Size(1454, 616);
             FilterResultGridView.TabIndex = 1;
             // 
             // SqlSeekerPage
@@ -184,12 +198,10 @@ namespace HandyTool.Tabs.SqlSeeker
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(TableLayoutMaster);
             Name = "SqlSeekerPage";
-            Size = new Size(1430, 633);
+            Size = new Size(1466, 668);
             TableLayoutMaster.ResumeLayout(false);
             TableLayoutTitle.ResumeLayout(false);
             TableLayoutTitle.PerformLayout();
-            TableLayoutFilter.ResumeLayout(false);
-            TableLayoutFilter.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)FilterResultGridView).EndInit();
             ResumeLayout(false);
         }
@@ -202,7 +214,8 @@ namespace HandyTool.Tabs.SqlSeeker
         private CheckBox CheckBoxFunctions;
         private TableLayoutPanel TableLayoutMaster;
         private TableLayoutPanel TableLayoutTitle;
-        private TableLayoutPanel TableLayoutFilter;
         private DataGridView FilterResultGridView;
+        private ComboBox DatabaseComboBox;
+        private Button RefreshButton;
     }
 }
--- HandyTool.Presentation/Tabs/SqlSeekerPage.cs
+++ HandyTool.Presentation/Tabs/SqlSeekerPage.cs
@@ -3,15 +3,8 @@ using HandyTool.HandyTool.Infrastructure;
 using HandyTool.HandyTool.Presentation.Control;
 using HandyTool.HandyTool.Presentation.Resources;
 using Microsoft.Data.SqlClient;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using Timer = System.Windows.Forms.Timer;
 
 namespace HandyTool.Tabs.SqlSeeker
@@ -31,7 +24,6 @@ namespace HandyTool.Tabs.SqlSeeker
             SearchTimer = new Timer();
             SearchTimer.Interval = 500;
             SearchTimer.Tick += SearchTimer_Tick;
-
         }
 
         private void SearchBar_TextChanged(object sender, EventArgs e)
@@ -45,6 +37,42 @@ namespace HandyTool.Tabs.SqlSeeker
         {
             SearchTimer.Stop();
             PerformSearch(LastSearchText);
+        }
+
+        public void LoadTableNames()
+        {
+            string server = ContainerFormFromMain.GetSelectedServerName();
+            if (server != null)
+            {
+                connect = SqlConnectionHelper.InitializeSqlConnection(server);
+                try
+                {
+                    connect.Open();
+                    for (int i = DatabaseComboBox.Items.Count - 1; i > 0; i--)
+                    {
+                        DatabaseComboBox.Items.RemoveAt(i);
+                    }
+                    SqlCommand cmd = new SqlCommand(
+                     @"SELECT name 
+                      FROM sys.databases 
+                      WHERE state_desc = 'ONLINE' 
+                      AND name NOT IN ('master', 'tempdb', 'model', 'msdb')",
+                     connect);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        DatabaseComboBox.Items.Add(reader["name"].ToString());
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex, "Error Message"
+                            , MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void PerformSearch(string SearchText)
@@ -169,7 +197,135 @@ namespace HandyTool.Tabs.SqlSeeker
             sqlBuilder.AppendLine($"DECLARE @searchStoredProcedure int = {(searchStoredProcedure ? 1 : 0)}");
             sqlBuilder.AppendLine($"DECLARE @searchView int = {(searchView ? 1 : 0)}");
             sqlBuilder.AppendLine($"DECLARE @searchFunction int = {(searchFunction ? 1 : 0)}");
+            if (DatabaseComboBox.SelectedIndex > 0)
+            {
+                sqlBuilder.AppendLine($"SET @databaseName = '" + DatabaseComboBox.Text + "'");
+                sqlBuilder.AppendLine(@"
+                                        IF LEN(@sql) > 0
+                                            SET @sql = @sql + ' UNION ALL '
+    
+                                        -- Tables
+                                        IF @searchTable = 1
+                                            SET @sql = @sql + '
+                                            SELECT 
+                                                ''Table'' AS ObjectType,
+                                                ''' + @databaseName + ''' AS DatabaseName, 
+                                                s.name AS SchemaName, 
+                                                t.name AS ObjectName, 
+                                                ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + t.name AS FullObject, 
+                                                ISNULL((SELECT STRING_AGG(c.name, '', '')
+                                                       FROM [' + @databaseName + '].sys.columns c
+                                                       WHERE c.object_id = t.object_id), '''') AS Parameters,
+                                                ''SELECT TOP 100 * FROM '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + t.name AS Example
+                                            FROM [' + @databaseName + '].sys.tables AS t 
+                                            JOIN [' + @databaseName + '].sys.schemas AS s ON t.schema_id = s.schema_id 
+                                            WHERE t.type = ''U'' AND t.name LIKE ''' + @tableNameFilter + ''' '
+    
+                                        -- Stored Procedures
+                                        IF @searchStoredProcedure = 1
+                                        BEGIN
+                                            IF LEN(@sql) > 0 AND @searchTable = 1
+                                                SET @sql = @sql + ' UNION ALL '
+            
+                                            SET @sql = @sql + '
+                                            SELECT 
+                                                ''Stored Procedure'' AS ObjectType,
+                                                ''' + @databaseName + ''' AS DatabaseName, 
+                                                s.name AS SchemaName, 
+                                                p.name AS ObjectName, 
+                                                ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + p.name AS FullObject, 
+                                                ISNULL((SELECT STRING_AGG(params.name + '''', '', '')
+                                                       FROM [' + @databaseName + '].sys.parameters params
+                                                       WHERE params.object_id = p.object_id), '''') AS Parameters,
+                                                CASE 
+                                                    WHEN EXISTS (SELECT 1 FROM [' + @databaseName + '].sys.parameters WHERE object_id = p.object_id)
+                                                    THEN ''EXEC '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + p.name + '' '' + 
+                                                        ISNULL((SELECT STRING_AGG(params.name + '' = 2508'', '', '')
+                                                               FROM [' + @databaseName + '].sys.parameters params
+                                                               WHERE params.object_id = p.object_id), '''')
+                                                    ELSE ''EXEC '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + p.name
+                                                END AS Example
+                                            FROM [' + @databaseName + '].sys.procedures AS p 
+                                            JOIN [' + @databaseName + '].sys.schemas AS s ON p.schema_id = s.schema_id 
+                                            WHERE p.name LIKE ''' + @tableNameFilter + ''' '
+                                        END
+    
+                                        -- Views
+                                        IF @searchView = 1
+                                        BEGIN
+                                            IF LEN(@sql) > 0 AND (@searchTable = 1 OR @searchStoredProcedure = 1)
+                                                SET @sql = @sql + ' UNION ALL '
+            
+                                            SET @sql = @sql + '
+                                            SELECT 
+                                                ''View'' AS ObjectType,
+                                                ''' + @databaseName + ''' AS DatabaseName, 
+                                                s.name AS SchemaName, 
+                                                v.name AS ObjectName, 
+                                                ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + v.name AS FullObject, 
+                                                ISNULL((SELECT STRING_AGG(c.name, '', '')
+                                                       FROM [' + @databaseName + '].sys.columns c
+                                                       WHERE c.object_id = v.object_id), '''') AS Parameters,
+                                                ''SELECT TOP 100 * FROM '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + v.name AS Example
+                                            FROM [' + @databaseName + '].sys.views AS v 
+                                            JOIN [' + @databaseName + '].sys.schemas AS s ON v.schema_id = s.schema_id 
+                                            WHERE v.name LIKE ''' + @tableNameFilter + ''' '
+                                        END
+    
+                                        -- Functions
+                                        IF @searchFunction = 1
+                                        BEGIN
+                                            IF LEN(@sql) > 0 AND (@searchTable = 1 OR @searchStoredProcedure = 1 OR @searchView = 1)
+                                                SET @sql = @sql + ' UNION ALL '
+            
+                                            SET @sql = @sql + '
+                                            SELECT 
+                                                CASE f.type
+                                                    WHEN ''IF'' THEN ''Inline Table-valued Function''
+                                                    WHEN ''TF'' THEN ''Table-valued Function''
+                                                    WHEN ''FN'' THEN ''Scalar Function''
+                                                    ELSE ''Function''
+                                                END AS ObjectType,
+                                                ''' + @databaseName + ''' AS DatabaseName, 
+                                                s.name AS SchemaName, 
+                                                f.name AS ObjectName, 
+                                                ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + f.name AS FullObject, 
+                                                ISNULL((SELECT STRING_AGG(params.name + '' ('' + TYPE_NAME(params.user_type_id) + '')'', '', '')
+                                                       FROM [' + @databaseName + '].sys.parameters params
+                                                       WHERE params.object_id = f.object_id AND params.parameter_id > 0), '''') AS Parameters,
+                                                CASE 
+                                                    WHEN EXISTS (SELECT 1 FROM [' + @databaseName + '].sys.parameters WHERE object_id = f.object_id AND parameter_id > 0)
+                                                    THEN 
+                                                        CASE f.type
+                                                            WHEN ''FN'' THEN ''SELECT '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + f.name + ''('' + 
+                                                                ISNULL((SELECT STRING_AGG(''1'', '', '')
+                                                                      FROM [' + @databaseName + '].sys.parameters params
+                                                                      WHERE params.object_id = f.object_id AND params.parameter_id > 0), '''') + '')''
+                                                            ELSE ''SELECT * FROM '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + f.name + ''('' + 
+                                                                ISNULL((SELECT STRING_AGG(''1'', '', '')
+                                                                      FROM [' + @databaseName + '].sys.parameters params
+                                                                      WHERE params.object_id = f.object_id AND params.parameter_id > 0), '''') + '')''
+                                                        END
+                                                    ELSE 
+                                                        CASE f.type
+                                                            WHEN ''FN'' THEN ''SELECT '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + f.name + ''()''
+                                                            ELSE ''SELECT * FROM '' + ''' + @databaseName + ''' + ''.'' + s.name + ''.'' + f.name + ''()''
+                                                        END
+                                                END AS Example
+                                            FROM [' + @databaseName + '].sys.objects AS f 
+                                            JOIN [' + @databaseName + '].sys.schemas AS s ON f.schema_id = s.schema_id 
+                                            WHERE f.type IN (''FN'', ''IF'', ''TF'') AND f.name LIKE ''' + @tableNameFilter + ''' '
+                                        END
 
+                                    -- Add ORDER BY to the final query
+                                    IF LEN(@sql) > 0
+                                    BEGIN
+                                        SET @sql = 'SELECT * FROM (' + @sql + ') AS AllObjects ORDER BY ObjectType, DatabaseName, SchemaName, ObjectName'
+                                        EXEC sp_executesql @sql
+                                    END");
+                return sqlBuilder.ToString();
+
+            }
             sqlBuilder.AppendLine(@"
                                     DECLARE db_cursor CURSOR FOR
                                         SELECT name 
@@ -344,7 +500,7 @@ namespace HandyTool.Tabs.SqlSeeker
                             default:
                                 rowColor = cg.ColorWhite;
                                 break;
-                        } 
+                        }
                     }
                 }
 
@@ -352,6 +508,19 @@ namespace HandyTool.Tabs.SqlSeeker
                 {
                     cell.Style.BackColor = rowColor;
                 }
+            }
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            LoadTableNames();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (SearchBar.Text.Length > 0)
+            {
+                SearchBar_TextChanged(SearchBar, EventArgs.Empty);
             }
         }
     }
